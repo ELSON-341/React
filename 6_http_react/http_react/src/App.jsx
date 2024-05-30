cd33d0661d47e07b7e967d3a81b319fc463d8f64
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-05-30 14:40:21 -0300

    resgata dados do server

--- 6_http_react/http_react/src/App.jsx
+++ 6_http_react/http_react/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
+
 import './App.css'
 
 function App() {
+  // 1 - resgatando dados
+  const [products, setProducts] = useState([])
+  
+  useEffect(() => {
+
+    async function getData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    getData()
+  }, [])
+
+  // 2 - envio de dados
+  
 
   return (
     <>
-      
+      <h1>HTTP em React</h1>
+      {/* 1 - resgate de dados */}
+      {products.map(product => (
+        <ul key={product.id}>
+          <li>{product.name} - R$ {product.price}</li>
+        </ul>
+      ))}
     </>
   )
 }
