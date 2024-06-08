683e841964e0ee3426231a27968310f9948bc358
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-06-08 14:42:32 -0300

    link in Raact router

--- 7_react_router/router/src/main.jsx
+++ 7_react_router/router/src/main.jsx
@@ -3,23 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom'
+import Contact from "./routes/Contanct.jsx"
 
-import Contanct from './routes/Contanct.jsx'
+// 2 - página da erro
+import ErrorPage from './routes/ErrorPage.jsx'
+
+// 1 - configurando router
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom"
+
+// 3  - componente base
+import Home from './routes/Home.jsx'
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>
+    path:'/',
+    element : <App/>,
+    errorElement: <ErrorPage/>,
+    // 3  - componente base
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+      },
+      {
+        path: 'contact',
+        element: <Contact/>,
+      }
+    ]
   },
-  {
-    path: "contact",
-    element: <Contanct/>
-  }
+//   {
+//     path:'contact',
+//     element : <Contact/>
+//   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
   </React.StrictMode>,
+
 )
