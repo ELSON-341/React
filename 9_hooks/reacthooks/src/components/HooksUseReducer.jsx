16d17c24861758a5efe4c19aeb212d40ae05422a
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-07-07 14:48:23 -0300

    useReducer

--- 9_hooks/reacthooks/src/components/HooksUseReducer.jsx
+++ 9_hooks/reacthooks/src/components/HooksUseReducer.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useReducer, useState } from "react"
 
 const HooksUseReducer = () => {
     // 1 - começando com o useReducer
@@ -7,11 +7,52 @@ const HooksUseReducer = () => {
         return Math.floor(Math.random(state) * 100)
     })
 
+    const inititalTasks = [{
+      id: 1, text: "Fazer alguma coisa",
+      id: 2, text: "Fazer autra coisa"
+    }]
+
+    const taskTextReducer = (state, action) => {
+      switch(action.type) {
+        case 'ADD':
+          const newTask = {
+            id: Math.floor(Math.random() * 1000),
+            text: taskText
+          }
+          setText('')
+
+          return[...state, newTask]
+
+        case 'DELETE':
+          break
+        default:
+          state
+      }
+    }
+    
+    const [taskText, setText] = useState('')
+    const [task, dispatctTask] = useReducer(taskTextReducer, inititalTasks)
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      dispatctTask({type : 'ADD'})
+    }
+
   return (
     <div>
         <h2>useReducer</h2>
         <p>Número: {number}</p>
         <button type="button" onClick={dispatch}>Alúterar número</button>
+        <h3>Tarefas</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => (e.target.value)} value={taskText || ''}/>
+          <input type="submit" value="Envior" />
+        </form>
+        {task.map(task => (
+          <ul key={task.id}>
+            <li>{console.log(task.text)}</li>
+          </ul>
+        ))}
     </div>
   )
 }
