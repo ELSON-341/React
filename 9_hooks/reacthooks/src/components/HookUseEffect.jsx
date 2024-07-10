f0b393ae40d56db3d66790697952ccd1a613b1c0
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-07-10 16:29:31 -0300

    commit

--- 9_hooks/reacthooks/src/components/HookUseEffect.jsx
+++ 9_hooks/reacthooks/src/components/HookUseEffect.jsx
@@ -15,11 +15,19 @@ const HookUseEffect = () => {
     useEffect(() => {
         console.log('Serei executado apenas uma vez.');
     })
+
+    // 3 - array de dependecias com valores
+    const [anotherNumber, setAnotherNumber] = useState(0)
+
+    useEffect(() => {
+        console.log('Sou excutado apenas quando mada o anotherNumber')
+    })
   return (
     <div>
         <h2>UseEffect</h2>
         <p>Number: {number}</p>
         <button type="button" onClick={changeSomething}>executar</button>
+        <button type="button" onClick={()=> setAnotherNumber(prevState => prevState + 1)}>Mudar anptherNumber</button>
     </div>
   )
 }
